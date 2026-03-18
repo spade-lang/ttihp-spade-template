b6f6a7d108f0fa03d89711e99863d539b9814418
TheZoq2 <mail@fransskarman.com>
2026-03-18 12:33:34 +0100

    Port Spade action from ttsky

---
+++ test/gate_sim_defines.v
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+`define GL_TEST
+`define FUNCTIONAL
+`define SIM
+||||||| constructed fake ancestor
+=======
+`define GL_TEST
+`define FUNCTIONAL
+`define USE_POWER_PINS
+`define SIM
+`define UNIT_DELAY #1
+>>>>>>> Start working on Spade workflow
